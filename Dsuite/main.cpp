4b33f4bf1cd2bfb5c97194234f3b40bb78025f08
millanek <millanek@gmail.com>
2019-04-02 17:09:33 +0200

    Initial Commit

---
+++ Dsuite/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dsuite
+//
+//  Created by Milan Malinsky on 02/04/2019.
+//  Copyright © 2019 Milan Malinsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
